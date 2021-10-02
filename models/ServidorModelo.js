1c1d9bf5bdac6927fc0d369555c2937ab8de7827
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-10-02 17:39:37 -0500

    se crea nuevo proyecto, para no almacenar la carpeta node_module

---
+++ models/ServidorModelo.js
@@ -0,0 +1,41 @@
+
+// importo o traigo express para mi codigo (para poderlo usar)
+const express = require('express')
+
+//traer, importar la conexion
+const {conectarBD}=require('../database/conexion.js')
+
+//traer las rutas
+const rutas=require('../routes/rutas.js')
+
+class ServidorModelo {
+
+    constructor(){
+
+        this.app = express()
+        this.despertarBD()
+        this.enrutarPeticion()
+
+    }
+    encederServidor(){
+
+        this.app.listen(process.env.PUERTO,function(){
+            console.log("servidor encendido..."+process.env.PUERTO)
+        })        
+
+    }
+
+    enrutarPeticion(){   
+        
+        this.app.use('/',rutas)
+
+    }
+
+    despertarBD(){ 
+
+    conectarBD()  
+
+    }
+}
+
+module.exports=ServidorModelo
